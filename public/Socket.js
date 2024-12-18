8b1f932595846619bdeca165a033315a9602834d
songwongbin <bb201802@naver.com>
2024-12-18 19:57:31 +0900

    [fix] 소소한 주석 수정

--- public/Socket.js
+++ public/Socket.js
@@ -13,7 +13,7 @@ let stageTable = null;
 let itemTable = null;
 let highScore = null;
 
-/* 서버에서 "connection" 메세지를 받았을 때 실행할 로직 */
+/* 서버에서 "connection" 메세지를 받았을 때  */
 socket.on("connection", (data) => {
   console.log("connection: ", data); // [1] 서버에서 받은 데이터 출력
   userId = data.uuid; // [2] 서버가 만들어준 uuid를 유저 ID에 저장
@@ -22,7 +22,7 @@ socket.on("connection", (data) => {
   highScore = data.highScore;
 });
 
-// 서버에서 "response" 메세지를 받아 이를 res에 저장
+/* 서버에서 "response" 메세지를 받았을 때 */
 socket.on("response", (data) => {
   console.log("response : ", data);
 });
@@ -30,16 +30,6 @@ socket.on("response", (data) => {
 /* 클라이언트에서 서버로 이벤트 보내기 위한 함수 */
 // [1-1] 이벤트에 맞는 담당 핸들러 식별 위해 handlerId 매개변수 받고
 // [1-2] 이벤트에 대한 정보 알려주기 위해 payload 매개변수 받음
-// const sendEvent = (handlerId, payload) => {
-//   // [2] 서버에게 "event" 메세지를 보냄, helper.js 파일의 handleEvent 함수가 이를 받아 처리
-//   socket.emit("event", {
-//     userId,
-//     clientVersion: CLIENT_VERSION,
-//     handlerId,
-//     payload,
-//   });
-// };
-
 const sendEvent = (handlerId, payload) => {
   return new Promise((resolve, reject) => {
     socket.emit("event", {
@@ -48,7 +38,7 @@ const sendEvent = (handlerId, payload) => {
       handlerId,
       payload,
     });
-
+    // 해당 메세지에 대한 응답 바로 받는 일회성 소켓
     socket.once("response", (data) => {
       if (data.handlerId === handlerId) {
         resolve(data);
